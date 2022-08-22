e96ea1b05046f60f661ce66bd3433b1239f8ac01
Evgeny <eugeneyaroslavtsev@gmail.com>
2022-08-22 22:21:25 +0700

    Initial commit

---
+++ src/ru/academits/java/Main.java
@@ -0,0 +1,25 @@
+package ru.academits.java;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 8;
+        int y = x * 2;
+
+        System.out.println("y = " + y);
+
+        String s1 = "Hello " + "world";
+        String s2 = "Hello " + "world " + 300;
+
+        System.out.println("Содержится? - " + s1.contains("world"));
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println("s1 = s2? " + s1.equals(s2));
+
+
+        System.out.println("длина строки s2 = " + s2.length());
+
+
+    }
+}
