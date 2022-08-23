5f1f80bf0efab4bedb6852afae7d694e2f0b17ee
Evgeny <eugeneyaroslavtsev@gmail.com>
2022-08-23 18:49:53 +0700

    added class Person

---
+++ src/ru/academits/java/OOP/Person.java
@@ -0,0 +1,25 @@
+package ru.academits.java.OOP;
+
+public class Person {
+    private String name;
+    private String middleName;
+    private String familyName;
+
+    public Person(String name, String middleName, String familyName) {
+        this.name = name;
+        this.middleName = middleName;
+        this.familyName = familyName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+}
