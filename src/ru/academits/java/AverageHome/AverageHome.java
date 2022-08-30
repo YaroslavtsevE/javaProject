31deb4d36fd7971b32ccef9ec8353ea427a0cc5e
Evgeny <eugeneyaroslavtsev@gmail.com>
2022-08-30 22:55:49 +0700

    added average home

--- src/ru/academits/java/AverageHome/AverageHome.java
+++ src/ru/academits/java/AverageHome/AverageHome.java
@@ -1,21 +1,67 @@
-//package ru.academits.java.AverageHome;
-//
-//public class AverageHome {
-//
-//    public static void main(String[] args) {
-//
-//    }
-//        int[] numbers = {5, 8, 12, -18, -54, 84, -35, 17, 37};
-//
-//        double average = 0;
-//        if (numbers.length > 0)
-//        {
-//            double sum = 0;
-//            for (int j = 0; j < numbers.length; j++) {
-//                sum += numbers[j];
-//            }
-//            average = sum / numbers.length;
-//        }
-//}
+package ru.academits.java.AverageHome;
 
+import java.util.Scanner;
+
+public class AverageHome {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число диапазона: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Введите второе число диапазона: ");
+        int b = scanner.nextInt();
+
+        int i = a;
+        int sum = 0;
+
+        if (IsTrue(a, b)) {
+
+            while (i <= b) {
+                sum = sum + i;
+                ++i;
+            }
+            double average = sum / getCountFor(a, b);
+            System.out.println("Average = " + average);
+        } else {
+            System.out.println("ERROR");
+        }
+    }
+
+    public static double getCountFor(int a, int b) {
+        if (a > b) {
+            return (double) ((a - b) + 1);
+        }
+        return (double) ((b - a) + 1);
+    }
+
+    public static boolean IsTrue(int a, int b) {
+        if ((a < b) && (a > 1) && (b <= 17)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean IsEven(int a) {
+        if (a % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+//    public static int getEvenSum(int a) {
+//        for (int i = a; i <= b; i++) {
+//            if (IsEven(i)) {
+//                while (i <= b) {
+//                    sum = sum + i;
+//                    ++i;
+//                }
+//                double even = sum / getCountFor(a, b);
+//                System.out.println("Average = " + average);
+//
+//            }
+//        }
+//    }
+}
 
