be7b99abf8352c34a74aaf2b32f979f3131795d6
Evgeny <eugeneyaroslavtsev@gmail.com>
2022-08-30 20:31:50 +0700

    updated loop for

--- src/ru/academits/java/LoopForHome/LoopForHome.java
+++ src/ru/academits/java/LoopForHome/LoopForHome.java
@@ -1,25 +1,20 @@
 package ru.academits.java.LoopForHome;
 
 public class LoopForHome {
-{
     public static void main(String[] args) {
-        int a = 100;
-        boolean c = true;
-        boolean isDivisibleByFour = getIsDivisibleByFour(100);
+
+//        boolean isDivisibleByFour = getIsDivisibleByFour(100);
         for (int i = 100; i >= 1; i--) {
-            if (getIsDivisibleByFour(i) == c) {
-            System.out.print(i + " ");
+            if (getIsDivisibleByFour(i)) {
+                System.out.print(i + " ");
             }
         }
-}
-
-        public static boolean getIsDivisibleByFour(int a)
-
-    {
-        if (a % 4 == 0) {
-        return (boolean) (true);
-        }
-        else { return (boolean) (false);}
     }
-}
+
+    public static boolean getIsDivisibleByFour(int a) {
+        if (a % 4 == 0) {
+            return true;
+        }
+        return false;
+    }
 }
