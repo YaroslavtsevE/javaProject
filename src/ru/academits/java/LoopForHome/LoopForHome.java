69db87fc20529f9a5a166aa98ef5a6a2cedf2cec
Evgeny <eugeneyaroslavtsev@gmail.com>
2022-09-05 22:59:51 +0700

    fixed loopFor

--- src/ru/academits/java/LoopForHome/LoopForHome.java
+++ src/ru/academits/java/LoopForHome/LoopForHome.java
@@ -4,13 +4,13 @@ public class LoopForHome {
     public static void main(String[] args) {
 
         for (int i = 100; i >= 1; i--) {
-            if (IsDivisibleByFour(i)) {
+            if (isDivisibleByFour(i)) {
                 System.out.print(i + " ");
             }
         }
     }
 
-    public static boolean IsDivisibleByFour(int a) {
+    public static boolean isDivisibleByFour(int a) {
         if (a % 4 == 0) {
             return true;
         }
